a41163f648365d5cfd67a614a4d7f43fc10cc70c
Lyuboslav Karev <lyubo.karev@gmail.com>
2020-08-21 22:31:48 +0300

    Changed some game mechanics

--- src/game.rs
+++ src/game.rs
@@ -3,6 +3,8 @@ pub mod game {
 
     use crate::human_player::human_player::HumanPlayer;
     use crate::bot::bot::Bot;
+    use std::cmp::max;
+    use std::env::current_exe;
 
     pub const EMPTY_PLACE_VALUE: u8 = 0;
     pub const EMPTY_PLACE_SYMBOL: char = '_';
@@ -11,6 +13,8 @@ pub mod game {
 
     pub const GAME_BOARD_SIZE: (usize, usize) = (6, 7); //(rows, cols)
 
+
+    #[derive(Copy, Clone)]
     pub enum PlayerType{
         Human,
         Bot
@@ -31,6 +35,7 @@ pub mod game {
         is_game_playing: bool,
     }
 
+    #[derive(Copy, Clone)]
     pub enum MoveType {
         Horizontal,
         Vertical,
@@ -48,7 +53,7 @@ pub mod game {
                     for (k,v) in self.values_players.iter(){
                         match v.get_type(){
                             PlayerType::Human => temp_map.insert(*k, Box::new(HumanPlayer::new(v.get_board_value(), v.get_board_symbol()))),
-                            PlayerType::Bot => temp_map.insert(*k, Box::new(Bot::new(v.get_board_value(), v.get_board_symbol())))
+                            PlayerType::Bot => temp_map.insert(*k, Box::new(Bot::new(v.get_board_value(), v.get_board_symbol(), 1)))
                         };
                     };
                     temp_map
@@ -79,6 +84,17 @@ pub mod game {
         }
         fn get_values_players(&self) -> &HashMap<u8, Box<dyn Player>>{
             &self.values_players
+        }
+        pub fn get_next_player_to_move_id(&self) -> u8{
+            let mut placed = 0;
+            for row in self.field.iter(){
+                for cell in row.iter(){
+                    if *cell != EMPTY_PLACE_VALUE{
+                        placed+= 1;
+                    }
+                }
+            }
+            placed % self.values_players.len() as u8
         }
 
         //Public interactions
@@ -128,55 +144,85 @@ pub mod game {
             for row_iter_index in 0..GAME_BOARD_SIZE.0 {
                 for column_index in 0..GAME_BOARD_SIZE.1 {
                     let row_index = GAME_BOARD_SIZE.0 - row_iter_index - 1;
-
-                    let winning_move_tuple: (bool, Option<MoveType>) = self.is_there_winning_move_from((row_index, column_index));
-                    if self.field[row_index][column_index] != EMPTY_PLACE_VALUE &&
-                        winning_move_tuple.0 {
-                        return winning_move_tuple;
+                    if self.field[row_index][column_index] != self::EMPTY_PLACE_VALUE{
+                        let winning_move_tuple: (bool, Option<MoveType>) = self.is_there_winning_move_from((row_index, column_index));
+                        if self.field[row_index][column_index] != EMPTY_PLACE_VALUE &&
+                            winning_move_tuple.0 {
+                            return winning_move_tuple;
+                        }
                     }
                 }
             }
             (false, None)
         }
-        pub fn is_there_winning_move_from(&self, (row, column): (usize, usize)) -> (bool, Option<MoveType>) {
-            let mut vertical_win: bool = column <= (GAME_BOARD_SIZE.1 - WINNING_LENGTH as usize);
-            let mut horizontal_win: bool = row > (GAME_BOARD_SIZE.0 - WINNING_LENGTH as usize);
-            let mut right_diagonal_win: bool = vertical_win && horizontal_win;
-            let mut left_diagonal_win: bool = WINNING_LENGTH as usize <= column && horizontal_win;
 
-            for offset in 1..WINNING_LENGTH as usize {
-                if vertical_win && self.field[row][column] != self.field[row][column + offset] {
-                    vertical_win = false;
-                }
-                if horizontal_win && self.field[row][column] != self.field[row - offset][column] {
-                    horizontal_win = false;
-                }
-                if right_diagonal_win && self.field[row][column] != self.field[row - offset][column + offset] {
-                    right_diagonal_win = false;
-                }
-                if left_diagonal_win && self.field[row][column] != self.field[row - offset][column - offset] {
-                    left_diagonal_win = false;
-                }
-            }
+        pub fn get_max_connected_from(& self, row: usize, column: usize) -> (u8, MoveType){
+            let mut current_vertical: (u8, MoveType) = (1, MoveType::Vertical);
+            let mut current_horizontal: (u8, MoveType) = (1, MoveType::Horizontal);
+            let mut current_right_diagonal: (u8, MoveType) = (1, MoveType::RightDiagonal);
+            let mut current_left_diagonal: (u8, MoveType) = (1, MoveType::LeftDiagonal);
 
-            let win_type: Option<MoveType> = {
-                if vertical_win {
-                    Some(MoveType::Vertical)
-                } else if horizontal_win {
-                    Some(MoveType::Horizontal)
-                } else if left_diagonal_win {
-                    Some(MoveType::LeftDiagonal)
-                } else if right_diagonal_win {
-                    Some(MoveType::RightDiagonal)
-                } else {
-                    None
+            let mut holder: Vec<(u8, MoveType)> = vec!(current_vertical, current_horizontal, current_left_diagonal, current_right_diagonal);
+
+            for offset in 1..WINNING_LENGTH as usize{
+                if column + offset < GAME_BOARD_SIZE.1 && self.field[row][column] == self.field[row][column + offset] {
+                    holder[0].0 += 1;
+                }
+                if offset <= row && self.field[row][column] == self.field[row - offset][column] {
+                    holder[1].0 += 1;
+                }
+                if offset <= row && column + offset < GAME_BOARD_SIZE.1 && self.field[row][column] == self.field[row - offset][column + offset] {
+                    holder[2].0 += 1;
+                }
+                if offset <= row && offset <= column && self.field[row][column] == self.field[row - offset][column - offset] {
+                    holder[3].0 += 1;
                 }
             };
+            
+            let mut min = holder[0].clone();
+            for win in holder{
+                if win.0 > min.0{
+                    min = win;
+                }
+            }
+            (min.0, match min.1{
+                MoveType::Horizontal => MoveType::Horizontal,
+                MoveType::Vertical => MoveType::Vertical,
+                MoveType::LeftDiagonal => MoveType::LeftDiagonal,
+                MoveType::RightDiagonal => MoveType::RightDiagonal
+            })
+        }
+        pub fn get_max_connected_for_player(&self, player_id: u8) -> (u8, MoveType){
+            let mut max: (u8, MoveType) = (0,MoveType::Horizontal);
+            for row in self.field.iter().enumerate(){
+                for cell in row.1.iter().enumerate(){
+                    if *cell.1 == player_id{
+                        let current = self.get_max_connected_from(row.0, cell.0);
+                        if current.0 > max.0{
+                            max = current;
+                        }
+                    }
+                }
+            };
+            max
+        }
+        pub fn is_there_winning_move_from(&self, (row, column): (usize, usize)) -> (bool, Option<MoveType>) {
+            let is_vertical_win_possible: bool = column <= (GAME_BOARD_SIZE.1 - WINNING_LENGTH as usize);
+            let is_horizontal_win_possible: bool = row > (GAME_BOARD_SIZE.0 - WINNING_LENGTH as usize);
+            let is_right_diagonal_win_possible: bool = is_vertical_win_possible && is_horizontal_win_possible;
+            let is_left_diagonal_win_possible: bool = WINNING_LENGTH as usize <= column && is_horizontal_win_possible;
 
-            if vertical_win || horizontal_win || left_diagonal_win || right_diagonal_win {
-                (true, win_type)
-            } else {
-                (false, win_type)
+            if is_vertical_win_possible || is_horizontal_win_possible || is_right_diagonal_win_possible || is_left_diagonal_win_possible {
+                let max_connected_from: (u8, MoveType) = self.get_max_connected_from(row, column);
+                if max_connected_from.0 == WINNING_LENGTH {
+                    (true, Some(max_connected_from.1))
+                }
+                else{
+                    (false, None)
+                }
+            }
+            else{
+                (false, None)
             }
         }
         pub fn get_human_players_ids(&self) -> Vec<u8>{
@@ -340,6 +386,22 @@ pub mod game {
             gs.place_on_board(2, 1);
 
             assert_eq!(gs.check_for_win().0, true);
+        }
+
+        #[test]
+        fn test_check_for_connected_number(){
+            let mut gs: GameState = GameState::init();
+
+            gs.create_and_add_player('+');
+
+            gs.place_on_board(1, 1);
+            assert_eq!(gs.get_max_connected_from(5, 0).0, 1);
+
+            gs.place_on_board(1, 1);
+            assert_eq!(gs.get_max_connected_from(5, 0).0, 2);
+
+            gs.place_on_board(1, 1);
+            assert_eq!(gs.get_max_connected_from(5, 0).0, 3);
         }
     }
 }
